be613db17836851542a657e16bdb0037f97f3e37
daleyjones <djones_15@hotmail.com>
2023-07-03 15:04:30 +0100

    employyeDatabase add update

--- db/EmployeeDatabase.js
+++ db/EmployeeDatabase.js
@@ -52,25 +52,69 @@ class EmployeeDatabase extends Database {
             reject(err);
           }
           resolve(results);
-        });
+        }
+      );
     });
   }
 
-
   addDepartment(department) {
     return new Promise((resolve, reject) => {
-        this.db.query('INSERT INTO department SET?', {name: department.department_name} , (err, result) => {
-            if (err){
-                reject(err);
-            }
-
-            resolve('Department $(dep)
+      this.db.query('INSERT INTO department SET ?', { name: department.department_name }, (err, result) => {
+        if (err) {
+          reject(err);
         }
 
+        resolve(`Department ${department.department_name} added successfully`);
+      });
+    });
+  }
 
+  addRole(role) {
+    const roleData = {
+      title: role.title,
+      salary: role.salary,
+      department_id: role.department_id
+    };
+    return new Promise((resolve, reject) => {
+      this.db.query('INSERT INTO role SET ?', roleData, (err, result) => {
+        if (err) {
+          reject(err);
+        }
 
+        resolve(`Role ${role.title} added successfully`);
+      });
+    });
+  }
 
+  addEmployee(employee) {
+    const employeeData = {
+      first_name: employee.first_name,
+      last_name: employee.last_name,
+      role_id: employee.role_id,
+      manager_id: employee.manager_id
+    };
+    return new Promise((resolve, reject) => {
+      this.db.query('INSERT INTO employee SET ?', employeeData, (err, result) => {
+        if (err) {
+          reject(err);
+        }
 
+        resolve(`Employee ${employee.first_name} ${employee.last_name} added successfully`);
+      });
+    });
+  }
+
+  updateEmployeeRole(employee) {
+    const { employee_id, role_id } = employee;
+    return new Promise((resolve, reject) => {
+      this.db.query('UPDATE employee SET role_id = ? WHERE id = ?', [employee.role_id, employee.employee_id], (err, result) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(`Employee ${employee_id} role updated successfully`);
+      });
+    });
   }
 }
 
