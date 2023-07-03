fa4c3f08c0d7ed0875e87a7e6cab63dcffdf207a
daleyjones <djones_15@hotmail.com>
2023-07-03 14:02:05 +0100

    index.js updated commit

--- index.js
+++ index.js
@@ -120,4 +120,59 @@ const add_employee = () => {
     });
 };
 
+db.getEmployees().then((results) => {
+    const managerQuestion = AddEmployeeQuestions[3];
+    results.forEach((employee) => {
+        managerQuestion.choices.push({
+            value: employee.id,
+            name: employee.name
+        });
+    });
+    managerQuestion.choices.push({
+        value: null,
+        name: 'None'
+    });
+
+    inquirer
+        .prompt(AddEmployeeQuestions)
+        .then((response) => {
+            db.addEmployee(response).then((results) => {
+                console.log('\n', results, '\n');
+               
+                doMenuQuestions();
+            });
+        });
+});
+
+const update_role = () => {
+    db.getEmployees().then((results) => {
+        const employeeQuestions = UpdateEmployeeRoleQuestions[0];
+        results.forEach((employee) => {
+            employeeQuestions.choices.push({
+                value: employee.id,
+                name: employee.name
+            });
+        });
+
+        db.getRoles().then((results) => {
+            const roleQuestion = UpdateEmployeeRoleQuestions[1];
+            results.forEach((role) => {
+                roleQuestion.choices.push({
+                    value: role.id,
+                    name: role.title
+                });
+            });
+
+            inquirer
+                .prompt(UpdateEmployeeRoleQuestions)
+                .then((response) => {
+                    db.updateEmployeeRole(response).then((results) => {
+                        console.log('\n', results, '\n');
+                        doMenuQuestions();
+                    });
+                });
+        });
+    });
+};
+
 doMenuQuestions();
